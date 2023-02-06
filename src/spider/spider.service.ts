ab00bd996588e7a5bf0acf9e5521888dc3c99e34
何樟榆 <hezhangyu@etonetech.com>
2023-02-06 23:58:02 +0800

    爬虫

--- src/spider/spider.service.ts
+++ src/spider/spider.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSpiderDto } from './dto/create-spider.dto';
 import { UpdateSpiderDto } from './dto/update-spider.dto';
-import axios from 'axios';
-import cheerio from 'cheerio'
-// import * as cheerio from 'cheerio';
+import axios, { AxiosProxyConfig } from 'axios';
+import cheerio from 'cheerio';
 
-const request = require('request')
-const path = require('path')
-const fs = require('fs')
+const request = require('request');
+const path = require('path');
+const fs = require('fs');
 
-// 爬取外网，得用proxy，其中52841 端口号是来自Clash
-const config: object = {
-  proxy: 'http://127.0.0.1:50683',
+// 爬取外网，得用proxy，其中2010 端口号是来自Clash
+const proxyConfig: AxiosProxyConfig = {
+  protocol: 'http',
+  host: '127.0.0.1',
+  port: 2010,
 };
 
 @Injectable()
@@ -21,58 +22,50 @@ export class SpiderService {
   }
 
   async findAll() {
-    // await axios.get('https://www.google.com/').then(res => {
-    //   console.log(res.data)
-    // })
-    let num = 1
+    let num = 1;
     while (num <= 1) {
-      await this.loadImgs(num)
-      num++
+      await this.loadImgs(num);
+      num++;
     }
 
     return `This action returns all spider`;
   }
 
-  loadImgs(pageNum) {
-    request({ ...config, url: 'https://v2ex.com/recent?p=' + pageNum }, (err: any, res: any, body: any) => {
-      
-      const $ = cheerio.load(body)
+  async loadImgs(pageNum) {
+    const urls: string[] = [];
+    const body = await axios
+      .get('https://v2ex.com/recent?p=' + pageNum, { proxy: proxyConfig })
+      .then((res) => res.data)
+      .catch((err) => err);
+    const $ = cheerio.load(body);
 
-      const urls: string[] = [];
-        $('#Main .box .item .avatar').each(function (index, item) {
-        // console.log(this.attribs.src)
-        // 获取图片的集合
-        urls.push($(this).attr('src'))
-      })
-      console.log(urls);
-      
-      this.writeFile(urls)
-    })
+    $('#Main .box .item .avatar').each(function (index, item) {
+      // 获取图片的集合
+      urls.push($(this).attr('src'));
+    });
+
+    this.writeFile(urls);
   }
 
   // 写入本地 生成实体文件
   writeFile(urls: string[]) {
-    urls.forEach(async url => {
-      // const buffer = await axios.get(url, { responseType: 'arraybuffer' }).then( res => res.data );
-      // const buffer = await request({ ...config, url: url }, (err: any, res: any, body: any) => body)
-      await request({ ...config, url: url }, (err: any, res: any, body: any) => {
-        console.log(body)
-        return
-      
-        // 生成文件之前，先判断存放目录是否存在，否则会报错
-        const target_path = path.join(__dirname, '../images/v2ex/');
-        if (!fs.existsSync(target_path)) {
-          fs.mkdirSync(target_path)
-        }
+    urls.forEach(async (url) => {
+      const buffer = await axios
+        .get(url, { responseType: 'arraybuffer', proxy: proxyConfig })
+        .then((res) => res.data)
+        .catch((err) => err);
+      const target_path = path.join(__dirname, '../images/v2ex/');
 
-        const ws = fs.createWriteStream(
-          path.join(target_path, new Date().getTime() + '.png')
-        );
-        
+      // 生成文件之前，先判断存放目录是否存在，否则会报错
+      if (!fs.existsSync(target_path)) {
+        fs.mkdirSync(target_path);
+      }
 
-        ws.write(body);
-      })
-      
+      const ws = fs.createWriteStream(
+        path.join(target_path, new Date().getTime() + '.png'),
+      );
+
+      ws.write(buffer);
     });
   }
 
